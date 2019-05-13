d2c370036c1a9d1c6e4c914e6a1bfd1c34ea5c33
Maxim Belobrovik <maksim-belobrovik@rambler.ru>
2019-05-13 15:11:09 +0300

    task6

--- UI/photoPost.js
+++ UI/photoPost.js
@@ -1,14 +1,48 @@
+const modul = (function () {
+    return {
+        setUser(name) {
+            list._user = name;
+            View.setUser(list._user);
+           
+        },
+        addAll(posts) {
+            const error = list.addAll(posts);
+            View.showPosts(list.getPage(0, 10 - error.length));
+            View.addAuthors(posts);
+        },
+        removePost(id) {
+            if (list.remove(id)) {
+                View.removePost(id);
+            }
+        },
+        editPost(id, post) {
+            if (list.edit(id, post)) {
+                View.editPost(id, post);
+            }
+        },
+        addPost(post) {
+            if (list.add(post))
+                View.addPost(post);
+           
+        }
+    }
+})();
+
+
 class PostList {
     _posts;
+    _user;
 
     
 
-    constructor(postsList = []) {
+    constructor(postsList = [],user) {
         this._posts = postsList.slice();
+        this._user = user;
     }
 
     clear() {
         this._posts = [];
+        this._user="";
     }
 
     addAll(posts) {
@@ -25,10 +59,7 @@ class PostList {
         return posts;
     }
 
-    
-
-    static validate(photoPost){
-        var _validator={
+     static _validator={
          description: function(description){
             const MIN=10;
             const MAX=200;
@@ -46,14 +77,17 @@ class PostList {
             }
          }
     }
-       if(_validator.description(photoPost.description) && _validator.photoLink(photoPost.photoLink)){
+
+    static validate(photoPost){
+       
+       if(this._validator.description(photoPost.description) && this._validator.photoLink(photoPost.photoLink)){
        	return true;
       } else { 
           return false;
       }
     }  
     add(photoPost){
-    	photoPost.id=new Date().getMilliseconds();
+    	photoPost.id=new Date().getMilliseconds()-Math.random();
     	photoPost.createdAt=new Date();
        if(PostList.validate(photoPost)){
            photoPost.likes=[];
@@ -70,10 +104,7 @@ class PostList {
         });
     }
 
-   
-
-    getPage(skip=0,top=10,filterConfig={}){
-    var _filter={
+   static _filter={
         author: function(list,author){
            return list.filter(function(item){
                 return item.author.includes(author);
@@ -95,9 +126,12 @@ class PostList {
             });
         }
     } 
+
+    getPage(skip=0,top=10,filterConfig={}){
+    
       var sortedPosts=this._posts;
         Object.keys(filterConfig).forEach(function(field){
-          return sortedPosts=_filter[field](sortedPosts,filterConfig[field]);
+          return sortedPosts=this._filter[field](sortedPosts,filterConfig[field]);
         });
         sortedPosts.sort(function(a,b){
             return a.createdAt-b.createdAt;
@@ -108,9 +142,9 @@ class PostList {
     } 
 
     remove(id){
-       for( i=0;i<photoPosts.length;i++){
+       for(var i=0;i<photoPosts.length;i++){
             if(photoPosts[i].id===id){
-                      photoPosts.splice(index,1);
+                      photoPosts.splice(i,1);
                       return true;
                       }
         }
@@ -119,12 +153,12 @@ class PostList {
 
     edit(id,photoPost){
        var postToEdit=this.get(id);
-    	if(PostList.validate(postToEdit)){
-            if(photoPost.description!==null && typeof photoPost.description!= "undefined" ){
-                postToEdit.description=photoPost.description;
-            }
+    	if(PostList.validate(postToEdit)){            
             if(photoPost.photoLink!==null && typeof photoPost.photoLink!= "undefined"){
                 postToEdit.photoLink=photoPost.photoLink;
+            }
+            if(photoPost.description!==null && typeof photoPost.description!= "undefined" ){
+                postToEdit.description=photoPost.description;
             }
             if(photoPost.hashTags!==null && typeof photoPost.hashTags!= "undefined"){
                 postToEdit.hashTags=photoPost.hashTags;
@@ -143,7 +177,7 @@ class PostList {
     }
 }
 
-const photoPosts = [
+var photoPosts = [
     {
     id: '1',
     description: 'Best description ever №1',
@@ -151,16 +185,16 @@ const photoPosts = [
     author: 'Inan Ivanov',
     hashTags:['#abc','#rty','#uio'],
     likes:['Inan Ivanov','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture1.jpg'
+    photoLink: 'https://images.wallpaperscraft.ru/image/sobaka_kraska_bryzgi_morda_65140_2560x1600.jpg'
     },
     {
     id: '2',
     description: 'Best description ever №2',
     createdAt: new Date('2017-02-23T23:00:00'),
-    author: 'Inan Ivanov',
+    author: 'ABC',
     hashTags:['#qwe','#rty','#uio'],
     likes:[],
-    photoLink: 'https://randomsite.ru/randompicture2.jpg'
+    photoLink: 'https://avatars.mds.yandex.net/get-pdb/1357142/48d07be0-c325-48e3-ba17-70c375f2ca22/s1200?w'
     },
     {id: '3',
     description: 'Best description ever №3',
@@ -168,188 +202,144 @@ const photoPosts = [
     author: 'Inan Ivanov',
     hashTags:['#abc','#qwe','#uio'],
     likes:['ABC'],
-    photoLink: 'https://randomsite.ru/randompicture3.jpg'
-    },
-    {id: '4',
-    description: 'Best description ever №4',
-    createdAt: new Date('2018-01-26T20:00:00'),
-    author: 'Inan Ivanov',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture4.jpg'
-    },
-    {id: '5',
-    description: 'Best description ever №5',
-    createdAt: new Date('2017-12-12T19:00:00'),
-    author: 'Inan Ivanov',
-    hashTags:['#abc','#uio'],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture5.jpg'
-    },
-    {id: '6',
-    description: 'Best description ever №6',
-    createdAt: new Date('2018-10-11T18:00:00'),
-    author: 'QWERTY',
-    hashTags:['#uio'],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture6.jpg'
-    },
-    {id: '7',
-    description: 'Best description ever №7',
-    createdAt: new Date('2018-10-10T17:00:00'),
-    author: 'QWERTY',
-    hashTags:['#qwe','#rty','#uio'],
-    likes:['QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture7.jpg'
-    },
-    {id: '8',
-    description: 'Best description ever №8',
-    createdAt: new Date('2018-09-09T18:00:00'),
-    author: 'QWERTY',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['Inan Ivanov','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture8.jpg'
-     },
-     {id: '9',
-    description: 'Best description ever №9',
-    createdAt: new Date('2017-03-08T15:00:00'),
-    author: 'QWERTY',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture9.jpg'
-    },
-    {id: '10',
-    description: 'Best description ever №10',
-    createdAt: new Date('2018-01-22T14:00:00'),
-    author: 'QWERTY',
-    hashTags:['#abc'],
-    likes:['QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture10.jpg'
-    },
-    {id: '11',
-    description: 'Best description ever №11',
-    createdAt: new Date('2018-02-10T05:00:00'),
-    author: 'Joe Black',
-    hashTags:['#rty'],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture11.jpg'
-    },
-    {id: '12',
-    description: 'Best description ever №12',
-    createdAt: new Date('2017-10-30T12:00:00'),
-    author: 'Joe Black',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['Inan Ivanov'],
-    photoLink: 'https://randomsite.ru/randompicture12.jpg'
-    },
-    {id: '13',
-    description: 'Best description ever №13',
-    createdAt: new Date('2018-04-27T20:56:00'),
-    author: 'Joe Black',
-    hashTags:[],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture13.jpg'
-    },
-    {id: '14',
-     description: 'Best description ever №14',
-     createdAt: new Date('2018-08-13T21:00:43'),
-     author: 'Joe Black',
-     hashTags:['#qwe','#rty','#uio'],
-     likes:['QWERTY'],
-     photoLink: 'https://randomsite.ru/randompicture14.jpg'
-    },
-    {id: '15',
-    description: 'Best description ever №15',
-    createdAt: new Date('2017-02-21T15:22:00'),
-    author: 'Joe Black',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture15.jpg'
-    },
-    {id: '16',
-    description: 'Best description ever №16',
-    createdAt: new Date('2018-06-20T16:01:00'),
-    author: 'ABC',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:[],
-    photoLink: 'https://randomsite.ru/randompicture16.jpg'
-    },
-    {id: '17',
-    description: 'Best description ever №17',
-    createdAt: new Date('2018-10-20T14:03:25'),
-    author: 'ABC',
-    hashTags:['#rty','#uio'],
-    likes:[],
-    photoLink: 'https://randomsite.ru/randompicture17.jpg'
-    },
-    {id: '18',
-    description: 'Best description ever №18',
-    createdAt: new Date('2017-02-17T12:54:44'),
-    author: 'ABC',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['Inan Ivanov','Joe Black','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture18.jpg'
-     },
-    {id: '19',
-    description: 'Best description ever №19',
-    createdAt: new Date('2017-03-13T23:00:00'),
-    author: 'ABC',
-    hashTags:['#abc','#qwe','#uio'],
-    likes:['Inan Ivanov','Joe Black','QWERTY','ABC'],
-    photoLink: 'https://randomsite.ru/randompicture19.jpg'
-    },
-    {id: '20',
-    description: 'Best description ever №20',
-    createdAt: new Date('2018-07-21T10:00:00'),
-    author: 'ABC',
-    hashTags:['#abc','#qwe','#rty','#uio'],
-    likes:['ABC'],
-    photoLink: 'https://randomsite.ru/randompicture20.jpg'
-    },];
+    photoLink: 'http://www.setwalls.ru/download.php?file=201305/2560x1600/setwalls.ru-37717.jpg'
+    }   
+    ];
+
+var list = new PostList(photoPosts, '');
+
+
+class View {
+    _posts;
+    _user;
+    _authors;
+
+    static setUser(name) {
+        this._user = name;
+        if (this._user === "Guest") {
+            document.getElementById("add").textContent = "Guests cant add posts";
+            document.getElementById("username").textContent = "Log In";
+            document.getElementById("addButton").style.display="none";
+            return undefined;
+        }
+        var user = document.getElementById("username");
+        user.textContent = name;
+        return user;
+    }
+
+    static addPhotoPostToHtml(photoPost) {
+        const posts = document.getElementById("posts");
+        const temp = document.getElementById("temp");
+         if (document.getElementById(photoPost.id) != null)
+             return;
+
+        var post = document.importNode(temp.content, true);
+
+        post.querySelector('div[class="photo"]').id = photoPost.id;
+        post.getElementById('user').textContent = this._user;
+        var str = "";
+        for (var i = 0; i < photoPost.hashTags.length; i++) {
+            str = str + photoPost.hashTags[i] + " ";
+        }
+        post.getElementById('hashtags').textContent = str;
+        post.getElementById('description').textContent = photoPost.description+"  id:"+photoPost.id;
+        post.getElementById('photo').src = photoPost.photoLink;
+        var dd = photoPost.createdAt.getDate();
+        var mm = photoPost.createdAt.getMonth();     
+        var yy = photoPost.createdAt.getFullYear();
+        var hh = photoPost.createdAt.getHours();
+        var min = photoPost.createdAt.getMinutes();
+        if (min < 10) min = '0' + min;
+        var date = dd + "." + mm + "." + yy +" "+ hh + ":" + min;
+        post.getElementById('date').textContent = date;
+        if (photoPost.author !== this._user) {
+            post.getElementById('edit').style.display = "none";
+            post.getElementById('delete').style.display = "none";
+        }
+        posts.append(post);
+    }
+
+    static addPost(photoPost)  {
+        const posts = document.getElementById("posts");
+        const temp = document.getElementById("temp");
+         if (document.getElementById(photoPost.id) != null)
+             return;
+
+        var post = document.importNode(temp.content, true);
+
+        post.querySelector('div[class="photo"]').id = photoPost.id;
+        post.getElementById('user').textContent = this._user;
+        var str = "";
+        for (var i = 0; i < photoPost.hashTags.length; i++) {
+            str = str + photoPost.hashTags[i] + " ";
+        }
+        post.getElementById('hashtags').textContent = str;
+        post.getElementById('description').textContent = photoPost.description+"  id:"+photoPost.id;
+        post.getElementById('photo').src = photoPost.photoLink;
+        var dd = photoPost.createdAt.getDate();
+        var mm = photoPost.createdAt.getMonth();     
+        var yy = photoPost.createdAt.getFullYear();
+        var hh = photoPost.createdAt.getHours();
+        var min = photoPost.createdAt.getMinutes();
+        if (min < 10) min = '0' + min;
+        var date = dd + "." + mm + "." + yy +" "+ hh + ":" + min;
+        post.getElementById('date').textContent = date;
+        posts.append(post);
+        if(!this._authors.has(this._user)){
+             var elem = document.createElement('option');
+            elem.value = this._user;
+            (document.querySelector('datalist[id="authors"]')).appendChild(elem);
+        }
+        
+    }
+
+    static showPosts(posts) {
+        for (var i = 0; i < posts.length; i++) {
+            View.addPhotoPostToHtml(posts[i]);
+        }
+    }
+
+    static removePost(id) {
+        document.getElementById(id).remove();
+    }
+
+    static editPost(id, photoPost) {
+
+        var post = document.getElementById(id);
+        if (post != null) {
+            if (photoPost.description !== undefined)
+                post.querySelector('div[id="description"]').textContent = photoPost.description;
+            if (photoPost.photoLink !== undefined)
+                post.querySelector('img[id="photo"]').src = photoPost.photoLink;
+       
+            if (photoPost.hashTags !== undefined){
+                 var str = "";
+                 for (var i = 0; i < photoPost.hashTags.length; i++) {
+                 str = str + photoPost.hashTags[i] + " ";
+                }
+                post.querySelector('div[id="hashtags"]').textContent = str;
+            }
+         }
+    }
+   static addAuthors(posts) {
+        var array = [];
+        for (var i = 0; i < posts.length; i++)
+                array.push(posts[i].author);
+                var authorsList = Array.from(new Set(array));
+                this._authors=new Set(array)
+        var dl = document.createElement('datalist');
+        document.querySelector('p[id="list"]').replaceChild(dl, document.querySelector('datalist[id="authors"]'));
+        document.querySelector('datalist').id = "authors";
+        authorsList.forEach(author => {
+            var elem = document.createElement('option');
+            elem.value = author;
+            (document.querySelector('datalist[id="authors"]')).appendChild(elem);
+        })
+    }
+
+}
 
 
 
-let test = new PostList(photoPosts);
-console.log("let test = new PostList(photoPosts);");
-console.log("");
-console.log("");
-console.log("test._posts");
-console.log(test._posts);
-console.log("");
-console.log("");
-console.log("test.getPage(0, 10)");
-console.log(test.getPage(0, 10));
-console.log("test.getPage(10, 10)");
-console.log(test.getPage(10, 10));
-console.log("test.getPage(0, 10, {author: 'AB'})");
-console.log(test.getPage(0, 10, {author: 'AB'}));
-console.log("");
-console.log("");
-console.log("test.get('10')");
-console.log(test.get("10"));
-console.log('test.get(10)');
-console.log(test.get(10));
-console.log("");
-console.log("");
-console.log("PostList.validate({ id: '23', description: 'Nexus 10', createdAt: new Date('2019-03-08T21:57:49'), author: 'KillerMask', photoLink: 'http://photoportal.by/photos/18', hashTags: [], likes: [] })");
-console.log(PostList.validate({ id: '23', description: 'Nexus 10', createdAt: new Date('2019-03-08T21:57:49'), author: 'KillerMask', photoLink: 'http://photoportal.by/photos/18', hashTags: [], likes: [] }));
-console.log("PostList.validate({ id: '', description: 'Nexus 10', createdAt: new Date('2019-03-08T21:57:49'), author: 'KillerMask', photoLink: 'http://photoportal.by/photos/18', hashTags: [], likes: [] })");
-console.log(PostList.validate({ id: '', description: 'Nexus 10', createdAt: new Date('2019-03-08T21:57:49'), author: 'KillerMask', photoLink: 'http://photoportal.by/photos/18', hashTags: [], likes: [] }));
-console.log("");
-console.log("");
-console.log("test.add({ id: '23', description: 'Nexus 10', createdAt: new Date('2019-03-08T21:57:49'), author: 'KillerMask', photoLink: 'http://photoportal.by/photos/18', hashTags: [], likes: [] })");
-console.log(test.add({ id: '23', description: 'Nexus 10', createdAt: new Date('2019-08-08T21:57:49'), author: 'KillerMask', photoLink: 'http://photoportal.by/photos/18', hashTags: [], likes: [] }));
-console.log("");
-console.log("");
-console.log("test.get('1')");
-console.log(test.get("1"));
-console.log("test.edit('1', { photoLink: 'http://haradok.info/static/news/5/4565/preview.jpg,description:'Поменяли!'' })");
-console.log(test.edit('1', { photoLink: 'http://haradok.info/static/news/5/4565/preview.jpg', description: "zxcvbnm" }));
-console.log("test.get('1')");
-console.log(test.get("1"));
-console.log("");
-console.log("");
-console.log("test.clear();");
-test.clear();
-console.log("test._posts");
-console.log(test._posts);
+
+
